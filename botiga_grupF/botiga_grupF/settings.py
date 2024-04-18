824780c715902360f68a499f24cf554db487b09f
angel_ivanov <angelspsv@gmail.com>
2024-04-18 10:27:25 +0200

    ajustando rutas para ver si funciona

--- botiga_grupF/botiga_grupF/settings.py
+++ botiga_grupF/botiga_grupF/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'botiga_grupF.modules.cataleg.apps.CatalegConfig',
 ]
 
 MIDDLEWARE = [
