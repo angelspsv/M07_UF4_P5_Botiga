3b39cf77ea49e142758862feeac5fb47ddae6a6e
angel_ivanov <angelspsv@gmail.com>
2024-04-19 17:02:02 +0200

    hello world funciona

--- botiga_grupF/pagaments/views.py
+++ botiga_grupF/pagaments/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-
-# Create your views here.
+@api_view(['GET'])
+def hello_world_view(request):
+    message = "Hello, world!"
+    return Response({"message": message}, status=status.HTTP_200_OK)
