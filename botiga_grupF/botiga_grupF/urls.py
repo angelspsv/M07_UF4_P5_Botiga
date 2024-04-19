c8b9b97303814fdb21a07f7a3747907506925c73
angel_ivanov <angelspsv@gmail.com>
2024-04-19 16:52:19 +0200

    modificada ruta

--- botiga_grupF/botiga_grupF/urls.py
+++ botiga_grupF/botiga_grupF/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('cataleg.urls')),
+    path('cataleg/', include('cataleg.urls')),
 ]
