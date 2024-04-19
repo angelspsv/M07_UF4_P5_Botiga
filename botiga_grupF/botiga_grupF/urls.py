b1e5b3530eb1aa6f579963daec390b9dfd7aa795
angel_ivanov <angelspsv@gmail.com>
2024-04-19 16:00:36 +0200

    ajustant codi per fer hello world

--- botiga_grupF/botiga_grupF/urls.py
+++ botiga_grupF/botiga_grupF/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('botiga_grupF.modules.cataleg.urls')),
+    path('catalog/', include('cataleg.urls')),
 ]
