aef97bd32faac388da3af4daf62392baebe77a75
angel_ivanov <angelspsv@gmail.com>
2024-04-19 19:56:39 +0200

    haciendo funcionalidad update product

--- botiga_grupF/cataleg/urls.py
+++ botiga_grupF/cataleg/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('hello/', views.hello_world_view, name='hello_world'),
     path('create_product/', views.create_product, name='create_product'),
     path('delete_product/<str:pk>/', views.delete_product, name='delete_product'),
+    path('update_product/<str:pk>/', views.update_product, name='update_product'),
 ]
--- botiga_grupF/cataleg/views.py
+++ botiga_grupF/cataleg/views.py
@@ -7,12 +7,14 @@ from rest_framework.response import Response
 from .models import Catalogo
 from .serializers import ProductoSerializer
 
-
+# de prueba
 @api_view(['GET'])
 def hello_world_view(request):
     message = "Hello, world!"
     return Response({"message": message}, status=status.HTTP_200_OK)
 
+
+# funcionalidad crear producto
 @api_view(['POST'])
 def create_product(request):
 
@@ -35,6 +37,7 @@ def create_product(request):
     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+# funcionalidad eliminar producto
 @api_view(['DELETE'])
 def delete_product(request, pk):
 
@@ -45,3 +48,24 @@ def delete_product(request, pk):
     product.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+# funcionalidad actualizar producto
+@api_view(['UPDATE'])
+def update_product(request, pk):
+    if not request.data or not 'nombre' in request.data or not 'precio' in request.data:
+        return Response({"error": "Invalid request data"}, status=status.HTTP_400_BAD_REQUEST)
+
+    product = Catalogo.objects.get(pk=pk)
+    if product is None:
+        return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    product.nombre = request.data['nombre']
+    product.descripcion = request.data.get('descripcion', product.descripcion)
+    product.id_seccion = request.data.get('id_seccion', product.id_seccion)
+    product.precio = request.data['precio']
+    product.stock = request.data.get('stock', product.stock)
+    product.peso = request.data.get('peso', product.peso)
+    product.save()
+
+    serializer = ProductoSerializer(product)
+    return Response(serializer.data, status=status.HTTP_200_OK)
