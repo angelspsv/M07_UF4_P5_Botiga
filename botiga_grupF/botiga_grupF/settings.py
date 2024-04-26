dcf603ee62bbe4a860f54dc0c80461203deaee76
angel_ivanov <angelspsv@gmail.com>
2024-04-26 17:00:50 +0200

    actualitzat settings

--- botiga_grupF/botiga_grupF/settings.py
+++ botiga_grupF/botiga_grupF/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'botiga_grupF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'botiga',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
