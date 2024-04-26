697a295fe973c1ead5c878c2de3a06a5a4eba960
angel_ivanov <angelspsv@gmail.com>
2024-04-26 16:42:18 +0200

    actualizar info para la  bbdd

--- botiga_grupF/cataleg/models.py
+++ botiga_grupF/cataleg/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # tabla modelo de productos
+class CatalogoSecciones(models.Model):
+    id_seccion = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+
+
 class Catalogo(models.Model):
     id_producto = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=255)
@@ -9,3 +14,4 @@ class Catalogo(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     peso = models.IntegerField()
+
