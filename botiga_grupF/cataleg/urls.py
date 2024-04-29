9557ae622820ac52bb565e17aa6a0483145056e2
angel_ivanov <angelspsv@gmail.com>
2024-04-29 18:32:59 +0200

    afegit comentari a urls

--- botiga_grupF/cataleg/urls.py
+++ botiga_grupF/cataleg/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# rutes URL de l'aplicació Cataleg amb el seu nou
 urlpatterns = [
     path('hello/', views.hello_world_view, name='hello_world'),
     path('create_product/', views.create_product, name='create_product'),
