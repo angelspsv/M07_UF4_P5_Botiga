ad2443c03cd361e35f5208de686b1f82a1f230db
angel_ivanov <angelspsv@gmail.com>
2024-04-30 20:23:36 +0200

    afegida ruta confirmacio tarjeta

--- botiga_grupF/pagaments/urls.py
+++ botiga_grupF/pagaments/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
+# conjunto de rutas URL con el nombre de las rutas, las vistas...
 urlpatterns = [
     path('hello/', views.hello_world_view, name='hello_world'),
+    path('confirm_credit_card/', views.confirm_credit_card, name='confirm_credit_card'),
 ]
