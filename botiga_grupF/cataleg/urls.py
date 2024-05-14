6951de9ebe08f6b21a65bca2e4cfcf2d2c2aad65
angel_ivanov <angelspsv@gmail.com>
2024-05-14 14:18:08 +0200

    afegint comentaris

--- botiga_grupF/cataleg/urls.py
+++ botiga_grupF/cataleg/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 # rutes URL de l'aplicació Cataleg amb el seu nom
+# per crear, modificar o esborrar producte o seccio
 urlpatterns = [
     path('hello/', views.hello_world_view, name='hello_world'),
     path('create_product/', views.create_product, name='create_product'),
--- botiga_grupF/cataleg/views.py
+++ botiga_grupF/cataleg/views.py
@@ -22,13 +22,17 @@ def hello_world_view(request):
 
 @api_view(['GET', 'POST'])
 def create_product(request):
-
+    #POST, para crear nuevo producto,
+    # los datos se validan y guardan usando serializador
+    # si tot es correcto respuesta 201 created
     if request.method == 'POST':
         serializer = ProductoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    #GET, recupera todos los productos del catalogo y
+    # se serializan para devolver una respuesta en forma de lista de productos
     elif request.method == 'GET':
         catalogo = Catalogo.objects.all()
         serializer = ProductoSerializer(catalogo, many=True)
@@ -47,15 +51,18 @@ def products_cat(request):
 # funcionalidad eliminar producto funciona por el id del producto
 @api_view(['DELETE', 'GET'])
 def delete_product(request, pk):
+    # intenta recuperar el producto del catalogo por su id/pk, si no existe error 404
     try:
         product = Catalogo.objects.get(pk=pk)
     except Catalogo.DoesNotExist:
         return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
 
+    # GET permite ver el producto a eliminar
     if request.method == 'GET':
         serializer = ProductoSerializer(product)
         return Response(serializer.data)
 
+    # DELETE, elimina el producto y devuelve un mensaje de exito
     elif request.method == 'DELETE':
         product.delete()
         return Response({"message": f"Product with ID {pk} deleted successfully"})
@@ -64,6 +71,7 @@ def delete_product(request, pk):
 # funcionalidad eliminar seccion de producto. funciona por el id de la seccion
 @api_view(['DELETE', 'GET'])
 def delete_section(request, pk):
+    # intenta recuperar la seccion por su id/pk
     try:
         section = CatalogoSecciones.objects.get(pk=pk)
     except CatalogoSecciones.DoesNotExist:
@@ -73,6 +81,7 @@ def delete_section(request, pk):
         serializer = CatalogoSeccionesSerializer(section)
         return Response(serializer.data)
 
+    # DELETE, elimina el seccion y devuelve un mensaje de exito
     elif request.method == 'DELETE':
         section.delete()
         return Response({"message": f"Section with ID {pk} deleted successfully"})
@@ -86,15 +95,18 @@ def delete_section(request, pk):
 
 @api_view(['GET', 'PUT', 'PATCH'])
 def update_product(request, pk):
+    # GET, recupera el producto del catalogo por su ID y devuelve sus detalles
     if request.method == 'GET':
         product = Catalogo.objects.get(pk=pk)
         serializer = ProductoSerializer(product)
         return Response(serializer.data)
 
+    #si fallan estos campos retorna mensaje de error codigo 400 (solicitud incorrecta)
     elif request.method == 'PUT' or request.method == 'PATCH':
         if not request.data or not 'nombre' in request.data or not 'precio' in request.data:
             return Response({"error": "Invalid request data"}, status=status.HTTP_400_BAD_REQUEST)
 
+        # comprueba si el producto existe
         product = Catalogo.objects.get(pk=pk)
         if product is None:
             return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
@@ -103,6 +115,7 @@ def update_product(request, pk):
             seccion = CatalogoSecciones.objects.get(pk=request.data['id_seccion'])
             product.id_seccion = seccion
 
+        #PUT o PATCH, valida y actualiza los datos del producto
         product.nombre = request.data['nombre']
         product.descripcion = request.data.get('descripcion', product.descripcion)
         product.precio = request.data['precio']
@@ -114,7 +127,7 @@ def update_product(request, pk):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-# la funcion update_section actualiza las secciones
+# la funcion update_section actualiza las secciones ya existentes
 @api_view(['GET', 'PUT', 'PATCH'])
 def update_section(request, pk):
     if request.method == 'GET':
@@ -143,13 +156,16 @@ def update_section(request, pk):
 #funcio per crear seccions per poder clasificar els productes (taula CatalogoSecciones)
 @api_view(['GET', 'POST'])
 def create_section(request):
-
+    # POST, creamos una seccion nueva, si tot es correcto se guerda
+    # Si la validación es exitosa, devuelve la seccion creada y el codigo 201 (creado)
     if request.method == 'POST':
         serializer = CatalogoSeccionesSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    # GET, recupera todas las secciones del catálogo y
+    # las devuelve como una lista de datos serializados
     elif request.method == 'GET':
         catalogosecciones = CatalogoSecciones.objects.all()
         serializer = CatalogoSeccionesSerializer(catalogosecciones, many=True)
